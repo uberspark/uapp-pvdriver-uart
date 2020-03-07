1281e2bf1c371e1137e435b32d370065331564df
David Shepard <daveman1010220@gmail.com>
2020-03-07 16:25:37 -0500

    Additional documentation for the tar_builder script. Export the KERNEL var for other purposes.

--- src/linux/tar_builder.fish
+++ src/linux/tar_builder.fish
@@ -2,7 +2,7 @@
 
 # Generally speaking, this variable controls the kernel build process and tells
 # the build process which architecture to cross-compile to.
-set KERNEL kernel7
+set -x KERNEL kernel7
 
 # Delete the output directory, if it exists.
 if test -e ../my_kernel/
@@ -10,10 +10,12 @@ if test -e ../my_kernel/
 end
 
 # Create output directories.
+# TODO: copy over work from a previous effort that makes the module version string a variable.
 mkdir -p ../my_kernel/lib/modules/4.19.106-v7+
 mkdir -p ../my_kernel/overlays
 
 # Build kernel artifacts.
+# Adjust the -j parameter, based on your build system. For example, 24 = 16 (cores) * 1.5 for my system. Build time is about three minutes.
 make -j 24 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
 
 # Copy kernel image and overlays to output dirs.
@@ -30,6 +32,8 @@ pushd ../
 # Create the archive.
 tar cvzf my_kernel.tar.gz my_kernel/*
 
+# TODO: Make IP address configurable.
+# TODO: Actually modify the Pi's boot kernel using SSH commands. This is potentially a second script, after tar_builder is ran.
 # scp pi@172.26.230.171:./my_kernel.tar.gz /home/pi/my_kernel.tar.gz
 
 popd
