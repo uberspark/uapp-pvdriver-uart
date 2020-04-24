bc6bb0b8c2ff9d759808aa292a587afa34ff4109
Amit Vasudevan <amitvasudevan@acm.org>
2020-04-24 09:42:39 -0500

    PL011_test: revise target_send script to stay consistent with test message

--- src/PL011_test/target_send.py
+++ src/PL011_test/target_send.py
@@ -2,7 +2,7 @@
 import os
 try:
     f = os.open("/dev/uxmhfpvduart",os.O_RDWR)
-    os.write(f,"hello world!")
+    os.write(f,"hello world from UART!")
     os.close(f)
 except IOError as e:
     print(e.errno)
